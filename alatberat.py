232551100acefea1d11be68d2f696d3cea623414
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2024-03-22 08:37:53 +0700

    Update alatberat.py

---
+++ alatberat.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show():
+    # Kode untuk halaman "Monitoring Heavy Equipment"
+    st.header('Monitoring Alat Berat')
+    st.info("Halaman ini sedang dalam pembangunan. Silakan kembali lagi nanti.")
