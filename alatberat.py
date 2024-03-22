5a07908b726f318e24b9eb255f22f7b05b56e67d
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2024-03-22 08:56:28 +0700

    Delete alatberat.py

--- alatberat.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def show():
-    # Kode untuk halaman "Monitoring Heavy Equipment"
-    st.header('Monitoring Alat Berat')
-    st.info("Halaman ini sedang dalam pembangunan. Silakan kembali lagi nanti.")
