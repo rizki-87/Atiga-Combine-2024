aabcf8b41f29ec1e460338bcb7490b8668b4a8d1
rizki-87 <140106584+rizki-87@users.noreply.github.com>
2024-03-25 16:39:46 +0700

    Update alatberat.py

--- alatberat.py
+++ alatberat.py
@@ -1,5 +1,21 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
-def show():
-    st.title("Halaman Monitoring Alat Berat")
-    st.header("Under Construction")
+@st.cache_resource(ttl=300, show_spinner=True)
+def load_data(url):
+    try:
+        df = pd.read_csv(url)
+        df['TANGGAL'] = pd.to_datetime(df['TANGGAL'], dayfirst=True, errors='coerce')
+        return df
+    except Exception as e:
+        st.error(f"Gagal memuat data: {e}")
+        return pd.DataFrame()
+#############################################################################################################################################
+# import streamlit as st
+
+# def show():
+#     st.title("Halaman Monitoring Alat Berat")
+#     st.header("Under Construction")
